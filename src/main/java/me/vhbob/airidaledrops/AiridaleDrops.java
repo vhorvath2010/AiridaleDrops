2a3d7922cafc5563cf560c17e7d77e6186cee8c2
Vincent Horvath <vhorvath2010@gmail.com>
2020-12-12 13:27:11 -0500

    Initial commit

---
+++ src/main/java/me/vhbob/airidaledrops/AiridaleDrops.java
@@ -0,0 +1,11 @@
+package me.vhbob.airidaledrops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AiridaleDrops extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+    }
+}
